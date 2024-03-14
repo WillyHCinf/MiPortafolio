c70b928d5e090921fa4c14fe7bb683656ab999bb
Willy HC <162583077+WillyHCinf@users.noreply.github.com>
2024-03-13 23:41:53 -0400

    Add files via upload

---
+++ Willy HC - portafolio 2/funciones.js
@@ -0,0 +1,39 @@
+function mostrarbotones(){
+    var botones = document.getElementsByClassName("botones")[0];
+
+    if(botones.style.visibility == "hidden"){
+        botones.style.visibility = "visible";
+    }else{botones.style.visibility = "visible";}
+}
+
+function mostrarboton1(){
+    var boton1 = document.getElementsByClassName("boton1")[0];
+
+    if(boton1.style.visibility == "hidden"){
+        boton1.style.visibility = "visible";
+    }else{boton1.style.visibility = "visible";}
+}
+
+function mostrarboton2(){
+    var boton2 = document.getElementsByClassName("boton2")[0];
+
+    if(boton2.style.visibility == "hidden"){
+        boton2.style.visibility = "visible";
+    }else{boton2.style.visibility = "visible";}
+}
+
+function mostrarboton3(){
+    var boton3 = document.getElementsByClassName("boton3")[0];
+
+    if(boton3.style.visibility == "hidden"){
+        boton3.style.visibility = "visible";
+    }else{boton3.style.visibility = "visible";}
+}
+
+function mostrarboton4(){
+    var boton4 = document.getElementsByClassName("boton4")[0];
+
+    if(boton4.style.visibility == "hidden"){
+        boton4.style.visibility = "visible";
+    }else{boton4.style.visibility = "visible";}
+}
